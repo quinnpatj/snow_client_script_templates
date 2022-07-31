7f59c712fc03e7963602a959fec8b1a9a8774037
Patrick Quinn <quinnpatj@gmail.com>
2022-07-30 22:49:32 -0500

    Added comments

--- snow_cs_template-confirm_submit.js
+++ snow_cs_template-confirm_submit.js
@@ -1,8 +1,10 @@
 function onSubmit() {
-	//Type appropriate comment here, and begin script below
+	//Confirm details are included for P1 Incidents if the Incident creator is not a Major Incident Manager
 	if (g_form.getValue('impact') == 1 && g_form.getValue('urgency') == 1 && !g_user.hasRoleExactly('major_inc_mgr')) {
-		var ans = confirm(g_user.firstName + " - The customer is notified of all Priority-1 Incidents. Confirm basic information is included before submitting this P1 Incident.\n\nSelect OK to submit , or Cancel to return to the record.");
+        //Create a confirmation dialog window
+		var ans = confirm("Hello " + g_user.firstName + ",\n\nThe customer is notified of all Priority-1 Incidents. Confirm basic information is included before submitting this P1 Incident.\n\nSelect OK to submit , or Cancel to return to the record.");
 		if (!ans) {
+            //When 'Cancel' is selected, notate recommended fields
 			g_form.addInfoMessage("Incident is not submitted");
 			g_form.addInfoMessage("If base information is unavailable, use the 'Additional comments' field to document why it is missing.");
 			g_form.showFieldMsg('category', "Major Incident base field");
